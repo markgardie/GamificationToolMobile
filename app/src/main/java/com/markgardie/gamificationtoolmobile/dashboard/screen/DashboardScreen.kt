c59a8b642e9a15047d21d4f6fd4406aabf5072f6
markgardie <60879509+markgardie@users.noreply.github.com>
2024-10-23 20:44:21 +0300

    - move EmptyState and LoadingState to core to use them as reusable ui components - change DashboardScreen accordingly

--- app/src/main/java/com/markgardie/gamificationtoolmobile/dashboard/screen/DashboardScreen.kt
+++ app/src/main/java/com/markgardie/gamificationtoolmobile/dashboard/screen/DashboardScreen.kt
@@ -1,13 +1,10 @@
 package com.markgardie.gamificationtoolmobile.dashboard.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
@@ -15,10 +12,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.markgardie.gamificationtoolmobile.core.ui_components.EmptyState
+import com.markgardie.gamificationtoolmobile.core.ui_components.LoadingState
 import com.markgardie.gamificationtoolmobile.dashboard.model.PointSystem
 import com.markgardie.gamificationtoolmobile.dashboard.viewmodel.DashboardUiState
 import com.markgardie.gamificationtoolmobile.dashboard.viewmodel.DashboardViewModel
@@ -27,7 +26,7 @@ import com.markgardie.gamificationtoolmobile.dashboard.viewmodel.DashboardViewMo
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreenWrapper(
-    dashboardViewModel: DashboardViewModel
+    dashboardViewModel: DashboardViewModel = hiltViewModel()
 ) {
     val uiState by dashboardViewModel.uiState.collectAsStateWithLifecycle()
 
@@ -45,7 +44,7 @@ fun DashboardScreenWrapper(
                 innerPadding = innerPadding,
             )
             is DashboardUiState.Loading -> LoadingState(innerPadding = innerPadding)
-            else -> EmptyState(innerPadding = innerPadding)
+            else -> EmptyState(innerPadding = innerPadding, emptyMessage = "Немає балів")
         }
     }
 
@@ -75,42 +74,11 @@ fun DashboardScreen(
 @Composable
 fun PointSystemItem(pointSystem: PointSystem) {
     ListItem(
-        headlineText = { Text(pointSystem.name, fontWeight = FontWeight.Medium) },
-        supportingText = { Text("Value: ${pointSystem.value}") },
+        headlineContent = { Text(pointSystem.name, fontWeight = FontWeight.Medium) },
+        supportingContent = { Text("Value: ${pointSystem.value}") },
     )
 }
 
-@Composable
-fun LoadingState(
-    innerPadding: PaddingValues
-) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        CircularProgressIndicator()
-    }
 
-}
 
-@Composable
-fun EmptyState(
-    innerPadding: PaddingValues
-) {
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-        Text(text = "Немає балів")
-
-    }
-}
