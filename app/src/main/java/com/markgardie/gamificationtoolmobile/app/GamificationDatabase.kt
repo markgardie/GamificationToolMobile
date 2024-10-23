9f79f7b39cccc8f4a1d167144c882c3fd1496559
markgardie <60879509+markgardie@users.noreply.github.com>
2024-10-23 16:57:44 +0300

    add student model

--- app/src/main/java/com/markgardie/gamificationtoolmobile/app/GamificationDatabase.kt
+++ app/src/main/java/com/markgardie/gamificationtoolmobile/app/GamificationDatabase.kt
@@ -2,10 +2,11 @@ package com.markgardie.gamificationtoolmobile.app
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.markgardie.gamificationtoolmobile.auth.Student
 import com.markgardie.gamificationtoolmobile.dashboard.PointSystem
 
 @Database(
-    entities = [PointSystem::class],
+    entities = [PointSystem::class, Student::class],
     version = 1
 )
 abstract class GamificationDatabase : RoomDatabase() {
