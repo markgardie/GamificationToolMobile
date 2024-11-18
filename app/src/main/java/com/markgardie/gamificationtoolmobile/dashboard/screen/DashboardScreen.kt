effd3e358812a117c4889accfecebfffc66818cd
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-18 16:48:21 +0200

    add retry button

--- app/src/main/java/com/markgardie/gamificationtoolmobile/dashboard/screen/DashboardScreen.kt
+++ app/src/main/java/com/markgardie/gamificationtoolmobile/dashboard/screen/DashboardScreen.kt
@@ -10,9 +10,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -42,6 +46,14 @@ fun DashboardScreenWrapper(
         topBar = {
             TopAppBar(
                 title = { Text("Дашборд") },
+                actions = {
+                    IconButton(onClick = { dashboardViewModel.retry() }) {
+                        Icon(
+                            imageVector = Icons.Default.Refresh,
+                            contentDescription = "Оновити"
+                        )
+                    }
+                }
             )
         }
     ) { innerPadding ->
